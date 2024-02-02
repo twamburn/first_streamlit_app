a1f606effa7c2bd3873790971c8e37b36da8c283
twamburn <158593082+twamburn@users.noreply.github.com>
2024-02-02 10:33:04 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.text('🥣 Omega 3 Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinich, & Rocket Smoothie')
 streamlit.text('🐔 Hard-boiled Free-range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
