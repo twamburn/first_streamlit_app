9bee49d3b926358bfebe9be841ad67242ee174cc
twamburn <158593082+twamburn@users.noreply.github.com>
2024-02-02 09:46:04 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit 
 
 streamlit.title('My Perents New Healthy Diner')
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favorites')
 streamlit.text('Omega 3 Blueberry Oatmeal')
 streamlit.text('Kale, Spinich, & Rocket Smoothie')
 streamlit.text('Hard-boiled Free-range Egg')
